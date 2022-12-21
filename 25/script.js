1c3dfdc7655bf86b856300c87bae7f6e58e85944
Serhii Vinokhodov <vinokhodova.nadin@gmail.com>
2022-12-21 11:38:09 +0100

    add commit

--- 25/script.js
+++ 25/script.js
@@ -65,13 +65,13 @@ function Cars(carMake, model, yearOfRegistr, numberPlate) {
     this.yearOfRegistr = yearOfRegistr;
     this.numberPlate = numberPlate;
 
-    this.ownerOfCar = function(owner) {
-        if (owner.age >= 18) {
-            this.owner = owner;
-        } else {
-            console.log(`${owner.name} not yet 18!`)
-        }
-    }
+    // this.ownerOfCar = function(owner) {
+    //     if (owner.age >= 18) {
+    //         this.owner = owner;
+    //     } else {
+    //         console.log(`${owner.name} not yet 18!`)
+    //     }
+    // }
 
 }
 
@@ -79,6 +79,13 @@ Cars.prototype.infoAuto = function() {
     return `Auto ${this.carMake} model ${this.model}, ${this.yearOfRegistr} year of registration. Number plate ${this.numberPlate}. Owner of te car ${this.ownerOfCar}`
 };
 
+Cars.prototype.ownerOfCar = function(owner) {
+    if (owner.age >= 18) {
+        this.owner = owner;
+    } else {
+        console.log(`${owner.name} not yet 18!`)
+    }
+};
 // Cars.prototype.infoAuto = function () {
 //     if (this.owners.age >= 18) {
 //     return `Auto ${this.carMake} model ${this.model}, ${this.yearOfRegistr} year of registration. Number plate ${this.numberPlate}. Owner is ${this.owner.getInfo()}`

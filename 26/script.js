72b6609e624d2544b7a5ec9bc3f1161c2cff7780
Serhii Vinokhodov <vinokhodova.nadin@gmail.com>
2022-12-27 16:56:16 +0100

    add 26 task

--- 26/script.js
+++ 26/script.js
@@ -16,81 +16,102 @@
 // В результате необходимо отрендерить на странице таблицу с выводом данных о всех продуктах.
 
 let kitchenProducts = [
-    	{
-    		type: 'grater',
-    		price: 10
-    	},
-    	{
-    		type: 'pastry-bag',
-    		price: 25
-    	},
-    	{
-    		type: 'scale',
-    		price: 5
-    	},
-    	{
-    		type: 'whisk',
-    		price: 15
-    	}
-    ];
-    
-    let devicesProducts = [
-    	{
-    		type: 'desktop',
-    		price: [100,1000]
-    	},
-    	{
-    		type: 'laptop',
-    		price: [50,1500]
-    	},
-    	{
-    		type: 'smartphone',
-    		price: [80,2000]
-    	},
-    	{
-    		type: 'tablet',
-    		price: [20,1300]
-    	}
-    ];
-    
-    let cosmeticsProducts = [
-    	{
-    		type: 'blush',
-    		price: 100
-    	},
-    	{
-    		type: 'eyeshadow',
-    		price: 50
-    	},
-    	{
-    		type: 'lipstick',
-    		price: 80
-    	},
-    	{
-    		type: 'nail-polish',
-    		price: 200
-    	},
-    	{
-    		type: 'perfume',
-    		price: 300,
-    	}
-    ];
+	{
+		type: 'grater',
+		price: 10
+	},
+	{
+		type: 'pastry-bag',
+		price: 25
+	},
+	{
+		type: 'scale',
+		price: 5
+	},
+	{
+		type: 'whisk',
+		price: 15
+	}
+];
+
+let devicesProducts = [
+	{
+		type: 'desktop',
+		price: [100, 1000]
+	},
+	{
+		type: 'laptop',
+		price: [50, 1500]
+	},
+	{
+		type: 'smartphone',
+		price: [80, 2000]
+	},
+	{
+		type: 'tablet',
+		price: [20, 1300]
+	}
+];
+
+let cosmeticsProducts = [
+	{
+		type: 'blush',
+		price: 100
+	},
+	{
+		type: 'eyeshadow',
+		price: 50
+	},
+	{
+		type: 'lipstick',
+		price: 80
+	},
+	{
+		type: 'nail-polish',
+		price: 200
+	},
+	{
+		type: 'perfume',
+		price: 300,
+	}
+];
 
 
-    function Product(category, type, price) {
-        this.category = category;
-        this.type = type;
-        this.price = price;
-    }
+function Product(category, type, price) {
+	this.category = category;
+	this.type = type;
+	this.price = price;
+}
 
-    Product.prototype.render = function() {
-        return `<tr>
+Product.prototype.render = function () {
+	return `<tr>
         <td><img src="images/${this.category}/${this.type}.svg" alt="${this.type}" width="50" height="50"></td>
-        <td>grater</td>
-        <td>10 USD</td>
+        <td>${this.type}</td>
+        <td>${this.prise}</td>
     </tr>`
-    }
+}
 
+function renderProd(category, arrProduct) {
+	let prod = arrProduct
+		.map(function (product) {
+			return new Product(category, product.type, product.price)
+		})
+		.map(function (product) {
+			return product.render()
+		})
+		.join('');
+	return prod;
+}
 
-
-    document.write
+document.write(`
+	<table>
+		<thead>
+			<tr>
+				<th>Image</th>
+				<th>Name</th>
+				<th>Price</th>
+			</tr>
+		</thead>
+		<tbody>${renderProd('kitchen', kitchenProducts)}, ${renderProd('devices', devicesProducts)}, ${renderProd('cosmetics', cosmeticsProducts)} </tbody>
+	</table>
+`)
